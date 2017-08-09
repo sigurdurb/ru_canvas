e596834ffb383920ae97bb54890eb4638b7bf3d5
Sigurdur B <siggibald4@gmail.com>
2017-08-09 20:34:29 +0000

    adds gen_groupset_csv.py that retrives details about the groupsets groups and the users within those groups. Updates to README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ru_canvas
-Python scripts to use with the canvas api. 
+Python scripts for use by Teachers and TA's to interact with the canvas api.
+ 
+All the scripts use canvasapi in some form: https://github.com/ucfopen/canvasapi
+
+Some scripts also have costum made requests that are not yet in the canvasapi but use parts of the canvasapi methods for simplicity. 
+
+### Requirements:
+
+```
+pip install canvasapi
+pip install pandas 
+```
+
+
+## scripts:
+
+### gen_groupset_csv.py
+
+This script generates a detailed csv for your groupset with columns for 
+'GroupSet', 'GroupSetID', 'Group', 'GroupID', 'Student', 'StudentID'
+
+If you have any empty groups(groups with no students) it wont list those.
+Also, It will only list students that are in the groups within the groupset.
+Those students that have not entrolled or the teacher has not enrolled in a group within the groupset won't be listed.
+
+You need to:
+
+Get your api key/token from your profile->settings
+
+
+* API_KEY = "" 
+
+Get your api url. i.e. "https://{yourschool}.instructure.com/api/v1/" 
+
+* API_URL = ""
+
+Get the Course id from your course url https://reykjavik.instructure.com/courses/{yourcourseID}
+Enter it as an integer
+
+* COURSE_ID = 123
+
+Get your group set category name, Note: not the assignment-group
+You can find this category name on the Tabs of YourCourse->People page or at yourcanvasinstance.com/courses/{your_course_id}/users and look at the tab
+
+* GROUP_CATGR_NAME = ""
---
+++ gen_groupset_csv.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+from canvasapi import Canvas
+import pandas as pd
+'''Get your api key/token from your profile->settings'''
+API_KEY = ""
+API_URL = "https://yourschool.instructure.com/api/v1/"
+
+'''Course id is in your url; example: https://reykjavik.instructure.com/courses/{Course_ID}'''
+COURSE_ID = 254 # type int - a number like 254
+
+'''This is the groups-set/category name, Note: not the assignment-group
+This category name is on the tabs of YourCourse->People page or url:yourcanvasinstance.com/courses/{course_id}/users'''
+GROUP_CATGR_NAME = "V1-Group" # Insert your group set/category name here
+
+TO_CSV_FILE = "groupset_" + GROUP_CATGR_NAME + "_details.csv"
+
+def main():
+
+	# Set up our Canvas object
+	canvas = Canvas(API_URL, API_KEY)
+
+	# First, retrieve the Course object
+	course = canvas.get_course(COURSE_ID)
+
+	# Print the course's name
+	print(course.name, course.id)
+
+	list_of_group_cats = get_group_categories(course)
+
+	possible_groups = []
+	all_rows = []
+	for group_cat in list_of_group_cats:
+		possible_groups.append(group_cat.name)
+		if group_cat.name == GROUP_CATGR_NAME:
+			
+			print(group_cat.to_json())
+			groups = list_groups_in_group_category(course,group_cat.id)
+			for g in groups:
+				#print(g.to_json())
+				
+				'''list_membership also returns the id's of users for a group, but not their names'''
+				'''So instead we use list_users'''
+				#membership = g.list_membership()
+				users_in_group = g.list_users()
+				for u in users_in_group:
+					row = []
+					row.extend([group_cat.name, group_cat.id, g.name, g.id, u.name, u.id])
+					
+					all_rows.append(row)
+					#print(u.to_json())
+
+	if all_rows:
+		df = pd.DataFrame(all_rows, columns=['GroupSet', 'GroupSetID', 'Group', 'GroupID', 'Student', 'StudentID'])
+		df.to_csv(TO_CSV_FILE, index_label = "Nr")
+		print("Successfully created CSV:", TO_CSV_FILE)
+
+	else:
+		print("No Category found for:", GROUP_CATGR_NAME)
+		print("Possible groups include:", *possible_groups, sep='\n')
+
+def get_group_categories(course):
+
+	return course.list_group_categories()
+
+def list_groups_in_group_category(course, cat_id):
+
+	'''You can make your own custom request like this'''
+	from canvasapi.paginated_list import PaginatedList
+	from canvasapi.group import Group
+	groups_plist = PaginatedList(Group,course._requester,
+					'GET',
+					'group_categories/{}/groups'.format(cat_id)
+					,{})
+	return groups_plist
+	
+
+'''contrib: If you have your groupCategoryID/groupsetID 
+You could use
+canvas.get_group_category(category_id) # That returns single group_category object
+So that function is available if you want to select only the one you need
+'''
+
+if __name__ == '__main__':
+	main()
